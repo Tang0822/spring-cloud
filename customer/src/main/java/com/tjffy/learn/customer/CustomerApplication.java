fa915cc6baff288ab06051c0e211ca97034fd4cd
jftang3 <jftang3@iflytek.com>
2018-12-28 17:32:51 +0800

    spring-cloud-server update

--- customer/src/main/java/com/tjffy/learn/customer/CustomerApplication.java
+++ customer/src/main/java/com/tjffy/learn/customer/CustomerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -16,6 +17,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableConfigServer
 public class CustomerApplication {
 
 	public static void main(String[] args) {
